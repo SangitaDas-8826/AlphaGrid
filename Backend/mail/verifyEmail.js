a74786b30fa5ab357f5907dad26de7e6f5399d06
SangitaDas-8826 <sangitadas8826@gmail.com>
2026-03-09 12:40:31 +0530

    email sending with resend

--- Backend/mail/verifyEmail.js
+++ Backend/mail/verifyEmail.js
@@ -1,5 +1,4 @@
 import { Resend } from "resend";
-import 'dotenv/config'
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -8,7 +7,8 @@ const verifyEmail = async (token, email) => {
   const verifyLink = `https://alpha-grid-one.vercel.app/verify/${token}`;
 
   await resend.emails.send({
-    from: process.env.MAIL_USER,
+    from: "AlphaGrid <onboarding@resend.dev>",
+    to: email,
     subject: "Verify Email",
     html: `<a href="${verifyLink}">Verify your email</a>`
   });
