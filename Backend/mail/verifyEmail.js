a045e8df286c78e47520a81023480c7bf8c0d743
SangitaDas-8826 <sangitadas8826@gmail.com>
2026-07-07 12:46:53 +0530

    try to fix email

--- Backend/mail/verifyEmail.js
+++ Backend/mail/verifyEmail.js
@@ -1,48 +1,111 @@
-
 import SibApiV3Sdk from "sib-api-v3-sdk";
-import "dotenv/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const client = SibApiV3Sdk.ApiClient.instance;
 
-client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+client.authentications["api-key"].apiKey =
+process.env.BREVO_API_KEY;
 
-const emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+const api = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const verifyEmail = async (email, token) => {
 
-  console.log("BREVO KEY =", process.env.BREVO_API_KEY);
+    console.log("EMAIL =", email);
+    console.log("TOKEN =", token);
 
-  const emailData = {
-    sender: {
-      name: "Alpha Grid",
-      email: process.env.MAIL_USER,
-    },
-    to: [
-      {
-        email: email,
-      },
-    ],
-    subject: "Verify your Email",
+    try {
 
-    htmlContent: `
-      <h2>Verify Email</h2>
+        const response = await api.sendTransacEmail({
 
-      <p>Click below</p>
+            sender: {
+                email: process.env.MAIL_USER,
+                name: "Alpha Grid"
+            },
 
-      <a href="https://alpha-grid-one.vercel.app/verify/${token}">
-      Verify Email
-      </a>
-    `,
-    
-  };
-console.log("BREVO_API_KEY:", process.env.BREVO_API_KEY);
-console.log("MAIL_USER:", process.env.MAIL_USER);
-  const response = await emailApi.sendTransacEmail(emailData);
-  
-  console.log(response);
-};
+            to: [
+                {
+                    email
+                }
+            ],
+
+            subject: "Verify Email",
+
+            htmlContent: `
+            <h2>Verify Email</h2>
+
+            <a href="https://alpha-grid-one.vercel.app/verify/${token}">
+            Verify
+            </a>
+            `
+        });
+
+        console.log(response);
+
+    }
+
+    catch(err){
+
+        console.log("BREVO ERROR");
+
+        console.log(err.response?.body);
+
+        console.log(err);
+
+    }
+
+}
 
 export default verifyEmail;
+
+
+
+
+// import SibApiV3Sdk from "sib-api-v3-sdk";
+// import "dotenv/config";
+
+// const client = SibApiV3Sdk.ApiClient.instance;
+
+// client.authentications["api-key"].apiKey = process.env.BREVO_API_KEY;
+
+// const emailApi = new SibApiV3Sdk.TransactionalEmailsApi();
+
+// const verifyEmail = async (email, token) => {
+
+//   console.log("BREVO KEY =", process.env.BREVO_API_KEY);
+
+//   const emailData = {
+//     sender: {
+//       name: "Alpha Grid",
+//       email: process.env.MAIL_USER,
+//     },
+//     to: [
+//       {
+//         email: email,
+//       },
+//     ],
+//     subject: "Verify your Email",
+
+//     htmlContent: `
+//       <h2>Verify Email</h2>
+
+//       <p>Click below</p>
+
+//       <a href="https://alpha-grid-one.vercel.app/verify/${token}">
+//       Verify Email
+//       </a>
+//     `,
+    
+//   };
+// console.log("BREVO_API_KEY:", process.env.BREVO_API_KEY);
+// console.log("MAIL_USER:", process.env.MAIL_USER);
+//   const response = await emailApi.sendTransacEmail(emailData);
+  
+//   console.log(response);
+// };
+
+// export default verifyEmail;
 
 
 
